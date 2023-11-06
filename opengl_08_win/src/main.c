834034d87c1a80d141884fa64434e2bbc48e1765
a98c14 <selimyesilkaya@gmail.com>
2023-11-07 01:35:32 +0300

    add clear logs command

--- opengl_08_win/src/main.c
+++ opengl_08_win/src/main.c
@@ -32,6 +32,7 @@ int main(void)
     Vec2 circle_pos = vec2_zero();
 
     /* main loop */
+    bool32 has_logged = false;
     while (!glfwWindowShouldClose(window->glfw_window))
     {
         /* frame: init */
@@ -44,6 +45,11 @@ int main(void)
         draw_circle_filled(dc, circle_pos, 10, ColorWhite);
         circle_pos = lerp_vec2(circle_pos, mouse.world, time.dt * 8.0f);
         draw_text(dc, vec2(0, -20), string_pushf(frame_arena, "Sample Text. FPS: %d", (int)(1 / time.dt)), ColorWhite);
+        if(time.current_frame > 10 && !has_logged)
+        {
+            log_info("log test");
+            has_logged = true;
+        }
 
         /* frame: render */
         renderer_render(renderer, time.dt);
